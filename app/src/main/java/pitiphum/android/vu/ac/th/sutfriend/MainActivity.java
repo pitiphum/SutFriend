5c3ad868ec6958227e81e9c21b6c87f263b9de3d
pitiphum <pitiphum_pos@vu.ac.th>
2018-08-24 10:57:03 +0700

    Initial commit

---
+++ app/src/main/java/pitiphum/android/vu/ac/th/sutfriend/MainActivity.java
@@ -0,0 +1,13 @@
+package pitiphum.android.vu.ac.th.sutfriend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
