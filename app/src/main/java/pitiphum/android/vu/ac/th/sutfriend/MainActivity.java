389e6309a1cfcb34b008c1823c7e7b26a3ac254d
pitiphum <pitiphum_pos@vu.ac.th>
2018-08-24 16:53:59 +0700

    Finish 24Aug

--- app/src/main/java/pitiphum/android/vu/ac/th/sutfriend/MainActivity.java
+++ app/src/main/java/pitiphum/android/vu/ac/th/sutfriend/MainActivity.java
@@ -9,5 +9,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment ใช้ตอนเปิด app ขึ้นมาครั้งแรก
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment())
+                    .commit();
+        }
+
+
+    }  //  Main Method
+
+} // Main Class
