9ff23394bfb9c21fda63ae5c63fcf87b8de8626a
pitiphum <pitiphum_pos@vu.ac.th>
2018-08-25 14:46:56 +0700

    Finish 25Aug

--- app/src/main/java/pitiphum/android/vu/ac/th/sutfriend/RegisterFragment.java
+++ app/src/main/java/pitiphum/android/vu/ac/th/sutfriend/RegisterFragment.java
@@ -1,16 +1,27 @@
 package pitiphum.android.vu.ac.th.sutfriend;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class RegisterFragment extends Fragment{
-
+    private ImageView imageView;
+    private Uri uri;
+    private boolean aBoolean = true;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -19,15 +30,84 @@ public class RegisterFragment extends Fragment{
 //        Create Toolbar
         createToolbar();
 
+//        Avata Controller
+        avataController();
 
     }   // Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.itemUpload) {
+
+            checkUpload();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void checkUpload() {
+
+        MyAlert myAlert = new MyAlert(getActivity());
+
+        if (aBoolean) {
+            myAlert.normalDialog("Non Choose Image", "Please Choose Image");
+        }
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+            aBoolean = false;
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+                Bitmap reSizeBitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 480, true);
+                imageView.setImageBitmap(reSizeBitmap1);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            Toast.makeText(getActivity(), "Please Choose Image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void avataController() {
+        imageView = getView().findViewById(R.id.imvAvata);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Please Choose Image App"),5);
+
+            }
+        });
+
+    }
+
+    @Override // icon cloud
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
 
         ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.register);
-        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill all Blank");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("PiTi Coffee shop");
 
         ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
         ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -38,6 +118,9 @@ public class RegisterFragment extends Fragment{
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+
+        setHasOptionsMenu(true);
+
 
     }
 
